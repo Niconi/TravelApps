2b103cf581f8188ab83509584787c76892db8bf8
Sasha <frsrifqi57@gmail.com>
2025-12-31 23:19:06 +0700

    feat: Add main navigation component with conditional onboarding and sign-in screens, updating package dependencies.

--- src/navigation/mainNavigation.tsx
+++ src/navigation/mainNavigation.tsx
@@ -2,16 +2,44 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Onboarding from '../screens/Onboarding/onboarding';
 import SignIn from '../screens/Auth/signIn';
+import { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createNativeStackNavigator();
 
-export default function MainNavigation() {
+function MainNavigation() {
+  const [isFirstLaunch, setIsFirstLaunch] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const checkFirstLaunch = async () => {
+      const appData = await AsyncStorage.getItem('isFirstLaunch');
+      if (appData === null) {
+        setIsFirstLaunch(true);
+        AsyncStorage.setItem('isFirstLaunch', 'false');
+      } else {
+        setIsFirstLaunch(false);
+      }
+    };
+    checkFirstLaunch();
+  }, []);
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Onboarding" component={Onboarding} options={{headerShown: false}}/>
-        <Stack.Screen name="SignIn" component={SignIn} options={{headerShown: false}}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    isFirstLaunch != null && (
+      <NavigationContainer>
+        <Stack.Navigator>
+          {isFirstLaunch && (<Stack.Screen
+            name="Onboarding"
+            component={Onboarding}
+            options={{ headerShown: false }}
+          />)}
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
   );
 }
+
+export default MainNavigation;
